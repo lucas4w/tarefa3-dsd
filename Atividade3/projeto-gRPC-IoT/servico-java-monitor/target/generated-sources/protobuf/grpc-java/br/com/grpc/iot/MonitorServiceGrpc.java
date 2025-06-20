1e941047afdddd456a39985d377a2257f4f149d9
Lucas Pinheiro da Costa <cadastro.lpc@gmail.com>
2025-06-20 10:57:38 +0000

    Criação do pacote java "servico-java-monitor-1.0-SNAPSHOT.jar" com sucesso

---
+++ Atividade3/projeto-gRPC-IoT/servico-java-monitor/target/generated-sources/protobuf/grpc-java/br/com/grpc/iot/MonitorServiceGrpc.java
@@ -0,0 +1,305 @@
+package br.com.grpc.iot;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+
+/**
+ * <pre>
+ * Definição do nosso serviço de monitoramento.
+ * </pre>
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.64.0)",
+    comments = "Source: contrato.proto")
+@io.grpc.stub.annotations.GrpcGenerated
+public final class MonitorServiceGrpc {
+
+  private MonitorServiceGrpc() {}
+
+  public static final java.lang.String SERVICE_NAME = "MonitorService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<br.com.grpc.iot.SensorData,
+      br.com.grpc.iot.StatusResposta> getEnviarDadosSensorMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "EnviarDadosSensor",
+      requestType = br.com.grpc.iot.SensorData.class,
+      responseType = br.com.grpc.iot.StatusResposta.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.CLIENT_STREAMING)
+  public static io.grpc.MethodDescriptor<br.com.grpc.iot.SensorData,
+      br.com.grpc.iot.StatusResposta> getEnviarDadosSensorMethod() {
+    io.grpc.MethodDescriptor<br.com.grpc.iot.SensorData, br.com.grpc.iot.StatusResposta> getEnviarDadosSensorMethod;
+    if ((getEnviarDadosSensorMethod = MonitorServiceGrpc.getEnviarDadosSensorMethod) == null) {
+      synchronized (MonitorServiceGrpc.class) {
+        if ((getEnviarDadosSensorMethod = MonitorServiceGrpc.getEnviarDadosSensorMethod) == null) {
+          MonitorServiceGrpc.getEnviarDadosSensorMethod = getEnviarDadosSensorMethod =
+              io.grpc.MethodDescriptor.<br.com.grpc.iot.SensorData, br.com.grpc.iot.StatusResposta>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.CLIENT_STREAMING)
+              .setFullMethodName(generateFullMethodName(SERVICE_NAME, "EnviarDadosSensor"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  br.com.grpc.iot.SensorData.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  br.com.grpc.iot.StatusResposta.getDefaultInstance()))
+              .setSchemaDescriptor(new MonitorServiceMethodDescriptorSupplier("EnviarDadosSensor"))
+              .build();
+        }
+      }
+    }
+    return getEnviarDadosSensorMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static MonitorServiceStub newStub(io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<MonitorServiceStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<MonitorServiceStub>() {
+        @java.lang.Override
+        public MonitorServiceStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new MonitorServiceStub(channel, callOptions);
+        }
+      };
+    return MonitorServiceStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static MonitorServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<MonitorServiceBlockingStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<MonitorServiceBlockingStub>() {
+        @java.lang.Override
+        public MonitorServiceBlockingStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new MonitorServiceBlockingStub(channel, callOptions);
+        }
+      };
+    return MonitorServiceBlockingStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static MonitorServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<MonitorServiceFutureStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<MonitorServiceFutureStub>() {
+        @java.lang.Override
+        public MonitorServiceFutureStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new MonitorServiceFutureStub(channel, callOptions);
+        }
+      };
+    return MonitorServiceFutureStub.newStub(factory, channel);
+  }
+
+  /**
+   * <pre>
+   * Definição do nosso serviço de monitoramento.
+   * </pre>
+   */
+  public interface AsyncService {
+
+    /**
+     * <pre>
+     * O cliente envia um FLUXO (stream) de dados do sensor e, ao final,
+     * o servidor retorna um único StatusResposta.
+     * Esta é a definição de um "Client Streaming RPC".
+     * </pre>
+     */
+    default io.grpc.stub.StreamObserver<br.com.grpc.iot.SensorData> enviarDadosSensor(
+        io.grpc.stub.StreamObserver<br.com.grpc.iot.StatusResposta> responseObserver) {
+      return io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall(getEnviarDadosSensorMethod(), responseObserver);
+    }
+  }
+
+  /**
+   * Base class for the server implementation of the service MonitorService.
+   * <pre>
+   * Definição do nosso serviço de monitoramento.
+   * </pre>
+   */
+  public static abstract class MonitorServiceImplBase
+      implements io.grpc.BindableService, AsyncService {
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return MonitorServiceGrpc.bindService(this);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do asynchronous rpc calls to service MonitorService.
+   * <pre>
+   * Definição do nosso serviço de monitoramento.
+   * </pre>
+   */
+  public static final class MonitorServiceStub
+      extends io.grpc.stub.AbstractAsyncStub<MonitorServiceStub> {
+    private MonitorServiceStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected MonitorServiceStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new MonitorServiceStub(channel, callOptions);
+    }
+
+    /**
+     * <pre>
+     * O cliente envia um FLUXO (stream) de dados do sensor e, ao final,
+     * o servidor retorna um único StatusResposta.
+     * Esta é a definição de um "Client Streaming RPC".
+     * </pre>
+     */
+    public io.grpc.stub.StreamObserver<br.com.grpc.iot.SensorData> enviarDadosSensor(
+        io.grpc.stub.StreamObserver<br.com.grpc.iot.StatusResposta> responseObserver) {
+      return io.grpc.stub.ClientCalls.asyncClientStreamingCall(
+          getChannel().newCall(getEnviarDadosSensorMethod(), getCallOptions()), responseObserver);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do synchronous rpc calls to service MonitorService.
+   * <pre>
+   * Definição do nosso serviço de monitoramento.
+   * </pre>
+   */
+  public static final class MonitorServiceBlockingStub
+      extends io.grpc.stub.AbstractBlockingStub<MonitorServiceBlockingStub> {
+    private MonitorServiceBlockingStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected MonitorServiceBlockingStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new MonitorServiceBlockingStub(channel, callOptions);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do ListenableFuture-style rpc calls to service MonitorService.
+   * <pre>
+   * Definição do nosso serviço de monitoramento.
+   * </pre>
+   */
+  public static final class MonitorServiceFutureStub
+      extends io.grpc.stub.AbstractFutureStub<MonitorServiceFutureStub> {
+    private MonitorServiceFutureStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected MonitorServiceFutureStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new MonitorServiceFutureStub(channel, callOptions);
+    }
+  }
+
+  private static final int METHODID_ENVIAR_DADOS_SENSOR = 0;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final AsyncService serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(AsyncService serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_ENVIAR_DADOS_SENSOR:
+          return (io.grpc.stub.StreamObserver<Req>) serviceImpl.enviarDadosSensor(
+              (io.grpc.stub.StreamObserver<br.com.grpc.iot.StatusResposta>) responseObserver);
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  public static final io.grpc.ServerServiceDefinition bindService(AsyncService service) {
+    return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+        .addMethod(
+          getEnviarDadosSensorMethod(),
+          io.grpc.stub.ServerCalls.asyncClientStreamingCall(
+            new MethodHandlers<
+              br.com.grpc.iot.SensorData,
+              br.com.grpc.iot.StatusResposta>(
+                service, METHODID_ENVIAR_DADOS_SENSOR)))
+        .build();
+  }
+
+  private static abstract class MonitorServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    MonitorServiceBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return br.com.grpc.iot.Contrato.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("MonitorService");
+    }
+  }
+
+  private static final class MonitorServiceFileDescriptorSupplier
+      extends MonitorServiceBaseDescriptorSupplier {
+    MonitorServiceFileDescriptorSupplier() {}
+  }
+
+  private static final class MonitorServiceMethodDescriptorSupplier
+      extends MonitorServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final java.lang.String methodName;
+
+    MonitorServiceMethodDescriptorSupplier(java.lang.String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (MonitorServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new MonitorServiceFileDescriptorSupplier())
+              .addMethod(getEnviarDadosSensorMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
