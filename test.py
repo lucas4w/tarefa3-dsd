9f64d689aa93d4cf90c7c24518d128e91fad61b1
lucas4w <lucasdemoraes313@gmail.com>
2025-06-30 05:12:29 +0000

    update requirements.txt

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://localhost:8000/api/users/by-email/"
-params = {"email": "teste@email.com"}
-
-response = requests.get(url, params=params)
-response.raise_for_status()
-print(response.json())
